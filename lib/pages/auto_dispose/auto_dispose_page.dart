6ec9ce473569341e5413004a89bf022944d3fd01
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2024-10-25 14:19:28 +0700

    hapus code auto dispose

--- lib/pages/auto_dispose/auto_dispose_page.dart
+++ lib/pages/auto_dispose/auto_dispose_page.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/pages/auto_dispose/auto_dispose_provider.dart';
 
-// todo 2 implement provider auto dispose dengan ConsumerWidget (finish)
-
-class AutoDisposePage extends ConsumerWidget {
+class AutoDisposePage extends StatelessWidget {
   const AutoDisposePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final hello = ref.watch(autoDisposeHelloProvider);
+  Widget build(BuildContext contextf) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('AutoDisposeProvider'),
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          '$hello',
-          style: Theme.of(context).textTheme.headlineLarge,
+          'AutoDisposeProvider',
         ),
       ),
     );
